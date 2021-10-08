650b44a064613538f7bdf5a896ada6652eb1a01e
Tim <>
2021-10-08 12:45:34 +0200

    release 1.6.0

--- test/test.js
+++ test/test.js
@@ -26,11 +26,11 @@ describe('openapi-format tests', () => {
                 let configFile = null;
                 let configFileOptions = {};
                 let sortOptions = {sortSet: {}};
-                let sortComponentsOptions = {sortComponentsSet: {}}
+                let sortComponentsOptions = {sortComponentsSet: {}};
                 let sortFile = null;
                 let filterFile = null;
                 let sortComponentsFile = null;
-                let filterOptions = {filterSet: {}}
+                let filterOptions = {filterSet: {}};
                 let inputFilename = null;
                 let input = null;
 
@@ -39,7 +39,7 @@ describe('openapi-format tests', () => {
                     configFile = path.join(__dirname, test, 'options.yaml');
                     // configFileOptions = jy.load(fs.readFileSync(configFile, 'utf8'));
                     configFileOptions = sy.parse(fs.readFileSync(configFile, 'utf8'));
-                    configFileOptions.sort = !(configFileOptions['no-sort'])
+                    configFileOptions.sort = !(configFileOptions['no-sort']);
                     options = Object.assign({}, options, configFileOptions);
                 } catch (ex) {
                     // console.error('ERROR Load options.yaml', ex)
@@ -48,7 +48,7 @@ describe('openapi-format tests', () => {
                         configFile = path.join(__dirname, test, 'options.json');
                         configFileOptions = JSON.parse(fs.readFileSync(configFile, 'utf8'));
                         if (configFileOptions['no-sort'] && configFileOptions['no-sort'] === true) {
-                            configFileOptions.sort = !(configFileOptions['no-sort'])
+                            configFileOptions.sort = !(configFileOptions['no-sort']);
                             delete configFileOptions['no-sort'];
                         }
                         options = Object.assign({}, options, configFileOptions);
@@ -74,7 +74,7 @@ describe('openapi-format tests', () => {
                     } catch (ex) {
                         // No options found. defaultSort.json will be used
                         // console.error('ERROR Load customSort.json', ex)
-                        options.sortSet = require('../defaultSort.json')
+                        options.sortSet = require('../defaultSort.json');
                     }
                 }
 
@@ -95,7 +95,7 @@ describe('openapi-format tests', () => {
                     } catch (ex) {
                         // No options found. defaultSort.json will be used
                         // console.error('ERROR Load customSort.json', ex)
-                        options.filterSet = require('../defaultFilter.json')
+                        options.filterSet = require('../defaultFilter.json');
                     }
                 }
 
@@ -115,7 +115,7 @@ describe('openapi-format tests', () => {
                     } catch (ex) {
                         // No options found. defaultSort.json will be used
                         // console.error('ERROR Load customSort.json', ex)
-                        options.sortComponentsOptions = require('../defaultSortComponents.json')
+                        options.sortComponentsOptions = require('../defaultSortComponents.json');
                     }
                 }
 
@@ -162,24 +162,24 @@ describe('openapi-format tests', () => {
                 }
 
                 // Initialize data
-                let result = input
+                let result = input;
 
                 // Filter OpenAPI document
                 if (options.filterSet) {
                     const resFilter = openapiFormat.openapiFilter(result, options);
-                    if (resFilter.data) result = resFilter.data
+                    if (resFilter.data) result = resFilter.data;
                 }
 
                 // Sort OpenAPI document
                 if (options.sort === true) {
                     const resFormat = openapiFormat.openapiSort(result, options);
-                    if (resFormat.data) result = resFormat.data
+                    if (resFormat.data) result = resFormat.data;
                 }
 
                 // Rename title OpenAPI document
                 if (options.rename) {
                     const resRename = openapiFormat.openapiRename(result, options);
-                    if (resRename.data) result = resRename.data
+                    if (resRename.data) result = resRename.data;
                 }
 
                 try {
@@ -193,7 +193,7 @@ describe('openapi-format tests', () => {
                         fs.writeFileSync(outputFilename, output, 'utf8');
                     }
                 } catch (error) {
-                    console.log('error', error)
+                    console.log('error', error);
                 }
 
                 assert.deepStrictEqual(result, output);
--- util-log-output.js
+++ util-log-output.js
@@ -4,42 +4,42 @@ function infoOut(msg) {
 }
 
 function logOut(msg, verboseLevel) {
-    verboseLevel = (verboseLevel === true ? 1 : verboseLevel || 0) ;
+    verboseLevel = (verboseLevel === true ? 1 : verboseLevel || 0);
     if (verboseLevel >= 1) {
         console.warn(msg);
     }
 }
 
 function debugOut(msg, verboseLevel) {
-    verboseLevel = (verboseLevel === true ? 1 : verboseLevel || 0) ;
+    verboseLevel = (verboseLevel === true ? 1 : verboseLevel || 0);
     if (verboseLevel >= 2) {
         console.warn(msg);
     }
 }
 
 function infoTable(obj, verboseLevel) {
-    verboseLevel = (verboseLevel === true ? 1 : verboseLevel || 0) ;
+    verboseLevel = (verboseLevel === true ? 1 : verboseLevel || 0);
     if (verboseLevel >= 1) {
-        const keys = Object.keys(obj)
-        let maxKeyLength = 0
-        let maxValLength = 0
-        let table = ``
+        const keys = Object.keys(obj);
+        let maxKeyLength = 0;
+        let maxValLength = 0;
+        let table = ``;
         keys.map((key) => {
-            const keyLength = String(key).length
-            const valStr = Array.isArray(obj[key]) ? obj[key].join(', ') :  String(obj[key])
-            const valLength = valStr.length
-            maxKeyLength = (keyLength > maxKeyLength) ? keyLength : maxKeyLength
-            maxValLength = (valLength > maxValLength) ? valLength : maxValLength
-        })
+            const keyLength = String(key).length;
+            const valStr = Array.isArray(obj[key]) ? obj[key].join(', ') : String(obj[key]);
+            const valLength = valStr.length;
+            maxKeyLength = (keyLength > maxKeyLength) ? keyLength : maxKeyLength;
+            maxValLength = (valLength > maxValLength) ? valLength : maxValLength;
+        });
         keys.map((key) => {
-            const keyLength = String(key).length
-            const valStr = Array.isArray(obj[key]) ? obj[key].join(', ') :  String(obj[key])
-            const valLength = valStr.length
-            const remainingKeyLength = maxKeyLength - keyLength
-            const remainingValLength = maxValLength - valLength
-            table += `| ${key}${(' ').repeat(remainingKeyLength)} | ${valStr}${(' ').repeat(remainingValLength)} |\n`
-        })
-        let header = `|-${('-').repeat(maxKeyLength)}-|-${('-').repeat(maxValLength)}-|\n`
+            const keyLength = String(key).length;
+            const valStr = Array.isArray(obj[key]) ? obj[key].join(', ') : String(obj[key]);
+            const valLength = valStr.length;
+            const remainingKeyLength = maxKeyLength - keyLength;
+            const remainingValLength = maxValLength - valLength;
+            table += `| ${key}${(' ').repeat(remainingKeyLength)} | ${valStr}${(' ').repeat(remainingValLength)} |\n`;
+        });
+        let header = `|-${('-').repeat(maxKeyLength)}-|-${('-').repeat(maxValLength)}-|\n`;
         return (header + table + header);
     }
 }
