4ddfb4c901ee59919630f12fc3e9da000050fd86
Tim <>
2021-12-22 14:09:12 +0100

    CLI - added casing file option

--- bin/cli.js
+++ bin/cli.js
@@ -18,6 +18,7 @@ program
     .description('Format a OpenAPI document by ordering and filtering fields.')
     .option('-o, --output <output>', 'write the formatted OpenAPI to an output file path.')
     .option('-s, --sortFile <sortFile>', 'the file with the sort priority options.')
+    .option('-c, --casingFile <filterFile>', 'the file with the casing options.')
     .option('-f, --filterFile <filterFile>', 'the file with the filter options.')
     .option('-c, --configFile <configFile>', 'the file with the OpenAPI-format CLI options.')
     .option('--no-sort', 'dont sort the OpenAPI file')
@@ -130,6 +131,21 @@ async function run(oaFile, options) {
         }
     }
 
+    // apply change casing by casing file if present
+    if (options && options.casingFile) {
+        infoOut(`- Casing file:\t\t${options.casingFile}`) // LOG - Casing file
+        try {
+            let casingOptions = {casingSet: {}}
+            casingOptions.casingSet = sy.parse(fs.readFileSync(options.casingFile, 'utf8'));
+            options = Object.assign({}, options, casingOptions);
+        } catch (err) {
+            console.error('\x1b[31m', `Casing file error - no such file or directory "${options.casingFile}"`)
+            if (options.verbose >= 1) {
+                console.error(err)
+            }
+        }
+    }
+
     infoOut(`- Input file:\t\t${oaFile}`) // LOG - Input file
 
     // Get
@@ -150,6 +166,12 @@ async function run(oaFile, options) {
     // Format & Order OpenAPI document
     if (options.sort === true) {
         const resFormat = await openapiFormat.openapiSort(res, options);
+        if (resFormat.data) res = resFormat.data
+    }
+
+    // Change case OpenAPI document
+    if (options.casingSet === true) {
+        const resFormat = await openapiFormat.openapiChangeCase(res, options);
         if (resFormat.data) res = resFormat.data
     }
 
