4583745f16c370a956ef806434c63d598d46361a
Tim Haselaars <timhaselaars@gmail.com>
2025-07-04 09:01:23 +0200

    fix: ignore casing when sorting component properties

--- test/test.js
+++ test/test.js
@@ -16,7 +16,7 @@ const tests = !localTesting
   ? fs.readdirSync(__dirname).filter(file => {
       return fs.statSync(path.join(__dirname, file)).isDirectory() && !file.startsWith('_');
     })
-  : ['yaml-sort-required'];
+  : ['yaml-sort-component-props'];
 
 describe('openapi-format tests', () => {
   let consoleLogSpy, consoleWarnSpy;
